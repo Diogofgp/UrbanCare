d596165689b9a3dcb35516934a9a8cebdcb96ad9
diogofgp <dfilipepereira@ipvc.pt>
2021-03-16 12:10:27 +0000

    Primeiro commit, layouts basicos

---
+++ app/src/main/java/com/example/urbancare/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.urbancare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //move to note list activity
+    fun moverParaNotas(view: View) {
+        val intent = Intent(this, NotasActivity::class.java)
+        startActivity(intent)
+    }
+
+}
